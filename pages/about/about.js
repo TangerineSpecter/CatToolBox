d3901b599a9d4b9db3d19c5c7958dfff91e2396b
TangerineSpecter <993033472@qq.com>
2018-03-09 23:55:49 +0800

    0.7.4 首页跳转bug修复

--- pages/about/about.js
+++ pages/about/about.js
@@ -6,7 +6,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-    version: '0.7.2',
+    version: '0.7.4',
     text: '小样，别碰我喵~'
   },
 
--- utils/config.js
+++
@@ -1,6 +0,0 @@
-var config = {
-  
-};
-
-//对外把对象config返回
-module.exports = config;
