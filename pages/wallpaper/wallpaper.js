67453a882d0cd6eddf26c291afc02ae42ec73ec0
TangerineSpecter <993033472@qq.com>
2018-03-18 00:51:26 +0800

    0.9.0 今日壁纸样式调整

--- pages/wallpaper/wallpaper.js
+++ pages/wallpaper/wallpaper.js
@@ -15,24 +15,9 @@ Page({
   onLoad: function (options) {
     var page = 1;
     var thisPage = this;
-    var images = [];
-    var url_head = 'https://cn.bing.com/';
     app.getWallpaperInfo(page, function (data) {
       if (data != null) {
-        for (var i = 0; i < data.images.length; i++) {
-          var wallPage = new Object();
-          //console.log(data);
-          //console.log(data.images[i])
-          var image = data.images[i];
-          wallPage.url = url_head + image.url;
-          wallPage.content = image.copyright;
-          wallPage.time = image.enddate;
-          wallPage.index = image.startdate;
-          images.push(wallPage);
-          console.log(images)
-          thisPage.setData({ wallPageList: images });
-          //console.log(thisPage.data.wallPageList)
-        }
+        thisPage.getImageData(data);
       } else {
         thisPage.onLoad();
       }
@@ -41,7 +26,61 @@ Page({
 
   //组装参数
   getImageData: function (data) {
+    var thisPage = this;
+    var images = [];
+    var url_head = 'https://cn.bing.com/';
+    for (var i = 0; i < data.images.length; i++) {
+      var wallPage = new Object();
+      var image = data.images[i];
+      wallPage.url = url_head + image.url;
+      wallPage.content = thisPage.getContent(image.copyright);
+      wallPage.author = thisPage.getAuchor(image.copyright);
+      wallPage.time = thisPage.getDate(image.enddate);
+      wallPage.index = thisPage.getIndex(image.enddate);
+      images.push(wallPage);
+      thisPage.setData({ wallPageList: images });
+      //console.log(thisPage.data.wallPageList)
+    }
+  },
 
+  //处理时间
+  getDate: function (date) {
+    var time = '';
+    time += date.substring(0, 4) + "-";
+    time += date.substring(4, 6) + "-";
+    time += date.substring(6);
+    return time;
+  },
+
+  //处理作者信息
+  getAuchor: function (data) {
+    return data.substring(data.indexOf('(') + 2, data.indexOf(')'));
+  },
+
+  //处理简介
+  getContent: function (data) {
+    return data.substring(0, data.indexOf('('));
+  },
+
+  //处理序号
+  getIndex: function (data) {
+    var index = 0;
+    var year = data.substring(0, 4);
+    var month = data.substring(4, 6);
+    var day = data.substring(6);
+    //console.log(new Date(2016, 2, 0).getDate());
+    return (this.getTotalDay(parseInt(year), parseInt(month)) + parseInt(day));
+  },
+
+  //处理总共时间
+  getTotalDay: function (year, month) {
+    var total = 0;
+    var thisPage = this;
+    total = total + new Date(year, month - 1, 0).getDate();
+    if (month > 0) {
+      thisPage.getTotalDay(month - 1);
+    }
+    return total;
   },
 
   /**
